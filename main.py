09eb26524104a941b441e7ba8dde71246e51d2bf
0gravity000 <0gravit000@gmail.com>
2022-04-21 17:44:38 +0900

    datastore_entityを追加し、ログイン、ログアウト処理を修正

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, session
 from flask_restful import reqparse, abort, Api, Resource
 import logging
 #from google.appengine.ext import ndb   これは2.x系 3.x系は使えない
@@ -20,14 +20,19 @@ from google.cloud import datastore
 import json
 from datetime import date, datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from datastore_entity import DatastoreEntity, EntityValue
+import secrets
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__, static_folder='./vuejs/dist/static', template_folder='./vuejs/dist')
+#app = Flask(__name__, static_folder='./vuejs/dist/static', static_url_path='/vuejs/dist/static')
 api = Api(app)  #flask_restful
 login_manager = LoginManager()  #flask_login
 login_manager.init_app(app) #flask_login
+secret = secrets.token_urlsafe(32)  #flask_login ランダムなセッションを生成
+app.secret_key = secret
 
 '''
 ダミーアカウント
@@ -100,28 +105,52 @@ class LoginRestful(Resource):
         #hashed_password=generate_password_hash(args["password"], method='sha256')
         #logging.debug(hashed_password)
         #remember = True if request.form.get('remember') else False
-        user = Account.fetch_account(args["email"])
+
+        user = Account().get_obj('email',args["email"])
+        #user = Account.fetch_account(args["email"])
         logging.debug(user)
         #logging.debug(user[0]["email"])
 
         # check if the user actually exists
         # take the user-supplied password, hash it, and compare it to the hashed password in the database
-        if not user or not check_password_hash(user[0]["password"], args["password"]):
+        if not user or not check_password_hash(user.password, args["password"]):
+        #if not user or not check_password_hash(user[0]["password"], args["password"]):
             # flash('Please check your login details and try again.')   #どうやるか要調査
             logging.debug('now leave Login post: auth NG')
-            return user, 401
+            return user.email, 401
             #return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
         # if the above check passes, then we know the user has the right credentials
+        # login_userにuserを渡すと、後は必要なことはflask-loginがやってくれます。
+        # 主にログイン状態の保持に必要な情報をセッションに保存しているのと、
+        # リクエストコンテキストのuserのアップデート、ログインシグナルの送信などをしてくれています
+        login_user(user)
         logging.debug('now leave Login post: auth OK')
-        return user, 201
+        return user.email, 201
         #return redirect(url_for('main.profile'))
 
+class LogoutRestful(Resource):
+    def get(self):
+        err = None
+        logging.debug('now in Logout get')
+        logout_user()
+        logging.debug('now leave Logout get')
+        #return redirect('TopView')
+        return "LogoutRestful"
+
+class TopRestful(Resource):
+    def get(self):
+        logging.debug('now in top get')
+        logging.debug('now leave top get')
+        return "TopRestful"
+      
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(AccountRestful, '/api/accounts')
 api.add_resource(LoginRestful, '/api/login')
+api.add_resource(LogoutRestful, '/api/logout')
+api.add_resource(TopRestful, '/api/top')
 
 # Cloud Datastore ################################
 datastore_client = datastore.Client()
@@ -156,13 +185,26 @@ def json_serial(obj):
     raise TypeError (f'Type {obj} not serializable')
 
 # Flask-login Model ###############################
-class Account(UserMixin):
-    id = ""
-    email = ""
-    password = ""
-    user_id = ""
-    created_at = ""
-    updated_at = ""
+class Account(DatastoreEntity, UserMixin):
+    id = EntityValue(None)
+    email = EntityValue(None)
+    password = EntityValue(None)
+    #user_id = EntityValue(None)
+    created_at = EntityValue(None)
+    updated_at = EntityValue(None)
+    #date_created = EntityValue(datetime.datetime.utcnow())
+    # specify the name of the entity kind.
+    # This is REQUIRED. Raises ValueError otherwise
+    __kind__ = "Accounts"
+
+    """
+    UserMixinを継承 メソッド get_id()
+    このメソッドは、このユーザーを一意に識別するstrを返す必要があり、
+    user_loaderコールバックからユーザーをロードするために使用できます。
+    これはstrでなければならないことに注意してください。
+    IDがネイティブにintまたはその他の型である場合は、strに変換する必要があります。
+    https://flask-login.readthedocs.io/en/latest/_modules/flask_login/mixins/#UserMixin
+    """
 
     def fetch_account(email):
         query = datastore_client.query(kind='Accounts')
@@ -177,16 +219,28 @@ class User(UserMixin):
     pass
 
 # Flask-login ###############################
+# user_loaderコールバックを提供する必要があります。 
+# このコールバックは、セッションに保存されているユーザーIDからユーザーオブジェクトをリロードするために使用されます。 
+# ユーザーのstr IDを取得し、対応するユーザーオブジェクトを返す必要があります。
 @login_manager.user_loader
-def load_user(email):
-    account = Account.fetch_account(email)
+def load_user(account_id):  #userをロードするためのcallback functionを定義
+    #load_userの引数は、Userクラスで定義したget_id()が返す値です。
+    account = Account().get_obj('id', account_id)
     return account
 
-# router
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/<path:path>', methods=('GET', 'POST'))
 def index(path):
     return render_template('index.html')
+
+'''   
+# router キャッチオールエンドポイント
+# https://flask.palletsprojects.com/en/2.1.x/patterns/singlepageapplications/
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("index.html")
+'''
 
 '''
 以下は動作しなかった
